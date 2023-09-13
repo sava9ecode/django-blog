56e6d4028ad82a9f5d86cc96d7f024fe15805a58
sava9ecode <sava9emode@yandex.ru>
2023-09-13 17:26:29 +0300

    Create models and superuser, change language

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Blog, BlogAuthor, BlogComment
+
+
+admin.site.register(Blog)
+admin.site.register(BlogAuthor)
+admin.site.register(BlogComment)
