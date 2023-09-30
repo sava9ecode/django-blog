00a05c06ba7da8961205e3e4a5ef908432cdfb2f
sava9ecode <sava9emode@yandex.ru>
2023-09-30 16:55:18 +0300

    Upgrade admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,45 @@ from django.contrib import admin
 from .models import Blog, BlogAuthor, BlogComment
 
 
-admin.site.register(Blog)
-admin.site.register(BlogAuthor)
-admin.site.register(BlogComment)
+class BlogInline(admin.TabularInline):
+    model = Blog
+    extra = 0
+
+
+class BlogCommentInline(admin.TabularInline):
+    model = BlogComment
+    extra = 0
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = (
+        "author",
+        "name",
+        "post_date",
+    )
+    list_filter = ("author", "post_date")
+    fields = (
+        "author",
+        "name",
+        "description",
+        "post_date",
+    )
+    inlines = [BlogCommentInline]
+
+
+@admin.register(BlogAuthor)
+class BlogAuthorAdmin(admin.ModelAdmin):
+    inlines = [BlogInline]
+
+
+@admin.register(BlogComment)
+class BlogCommentAdmin(admin.ModelAdmin):
+    list_display = ("description", "blog", "author", "post_date")
+    list_filter = ("author", "post_date")
+    fields = (
+        "author",
+        "blog",
+        "description",
+        "post_date",
+    )
